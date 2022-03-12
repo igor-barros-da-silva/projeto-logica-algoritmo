c89fb6409608bc0240c1b53f0baeca385053125b
Igor Barros <barrosigor74@gmail.com>
2022-03-11 22:59:50 -0300

    Operadores relacionais.

---
+++ projeto-logica-algoritimo/src/pacote/operadorRelacional/OperadorRelacional.java
@@ -0,0 +1,20 @@
+package pacote.operadorRelacional;
+
+public class OperadorRelacional {
+	
+	private static double n1 = 10.1;
+	private static double n2 = 20.1;
+	
+	public static void main(String[] args) {		
+				
+		if (n1 == n2) {
+			System.out.println("Números iguais!");
+		} else {
+			System.out.println("Números não são iguais!");
+		}
+		
+	}
+	
+	
+
+}
