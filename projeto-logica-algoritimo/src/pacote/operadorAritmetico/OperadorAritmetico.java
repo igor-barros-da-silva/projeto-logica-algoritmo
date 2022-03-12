88e6fdc908c67047e4e92849896987fdd6d805ff
Igor Barros <barrosigor74@gmail.com>
2022-03-11 22:34:48 -0300

    Operadores Aritméticos.

---
+++ projeto-logica-algoritimo/src/pacote/operadorAritmetico/OperadorAritmetico.java
@@ -0,0 +1,14 @@
+package pacote.operadorAritmetico;
+
+public class OperadorAritmetico {
+
+	public static void main(String[] args) {
+
+		double n1 = 50;
+		double n2 = 3;
+
+		double soma = n1 + n2;
+
+		System.out.println(soma);
+	}
+}
