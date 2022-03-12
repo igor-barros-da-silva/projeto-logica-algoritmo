80288b049a586296ca666f7bfb7ecfa645e81315
Igor Barros <barrosigor74@gmail.com>
2022-03-12 06:42:56 -0300

    Estruturas de Repetição.

---
+++ projeto-logica-algoritimo/src/pacote/estruturaRepeticao/EstruturaRepeticao.java
@@ -0,0 +1,14 @@
+package pacote.estruturaRepeticao;
+
+public class EstruturaRepeticao {
+	
+	public static void main(String[] args) {
+		
+		int n1 =0;
+		
+		while (n1 < 100) {
+			System.out.println(n1 +=1);
+		}
+	}
+
+}
