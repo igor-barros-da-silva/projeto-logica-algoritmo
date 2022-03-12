dd19cb4780f0580e39d21ba59236d17f61334ace
Igor Barros <barrosigor74@gmail.com>
2022-03-11 21:01:46 -0300

    O que são constantes?

---
+++ projeto-logica-algoritimo/src/pacote/constante/Constante.java
@@ -0,0 +1,8 @@
+package pacote.constante;
+
+public class Constante {
+
+	private static final String TIPO_JURIDICA = "Juridica";
+	private static final boolean ATIVO = true;
+	private static final int MEDIA = 80;
+}
