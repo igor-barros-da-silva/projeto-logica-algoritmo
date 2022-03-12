6c508c12e5d80aafece4dbede24b46151811e563
Igor Barros <barrosigor74@gmail.com>
2022-03-12 07:03:37 -0300

    O que é sintaxe e semântica do código?

---
+++ projeto-logica-algoritimo/src/pacote/estruturaRepeticao/ExemploSemantica.java
@@ -0,0 +1,21 @@
+package pacote.estruturaRepeticao;
+
+public class ExemploSemantica {
+
+	public static void main(String[] args) {
+
+		int incremento;
+
+		for (int cont = 0; cont < 10; cont++) {
+			incremento = 99;
+			System.out.println(incremento++);
+		}
+
+		// for (int cont = 0; cont < 10; cont++) {
+		// incremento = 99;
+		//
+		// }
+		// System.out.println(incremento++);
+	}
+
+}
